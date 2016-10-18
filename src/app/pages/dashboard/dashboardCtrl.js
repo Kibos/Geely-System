2f9828a1cee354ccdde99019d1a7175d87de2362
Kibos <759508922@qq.com>
2016-10-18 23:50:25 +0800

    角色

--- src/app/pages/dashboard/dashboardCtrl.js
+++ src/app/pages/dashboard/dashboardCtrl.js
@@ -15,5 +15,7 @@
     $scope.supplier = {id:'100502154',name:'浙江XX集团',person:'张三',phone:'1868989898',message:'供应商概览简介--------'};
 
     $scope.user=Auth.getCurrentUser();
+
+    $scope.roles={'admin':'网发','user':'代理商','gonguser':'供应厂商','deuser':'设计厂商'}
   }
 })();
