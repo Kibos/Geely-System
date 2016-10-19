4cb73246b4af35aa423ffe9646cd6baa7eba7428
81128786 <2451278782@qq.com>
2016-10-19 22:22:12 +0800

    yanshou

---
+++ src/app/pages/wangfa/acceptancegl/acceptancegl.module.js
@@ -0,0 +1,31 @@
+/**
+ * @author a.demeshko
+ * created on 1/12/16
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.wangfa.acceptancegl', [])
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('ba.jiandianguanli.acceptancegl', {
+        url: '/acceptancegl',
+        controller: 'acceptanceglCtrl',
+        templateUrl: 'app/pages/wangfa/acceptancegl/acceptancegl.html',
+          title: '验收管理',
+          sidebarMeta: {
+            icon: 'ion-ios-pulse',
+            order: 30,
+          },
+      })
+      .state('ba.jiandianguanli.acceptanceglcheck', {
+        url: '/acceptanceglcheck',
+        controller: 'acceptanceglcheckCtrl',
+        templateUrl: 'app/pages/wangfa/acceptancegl/acceptanceglcheck.html',
+
+      });
+  }
+})();
