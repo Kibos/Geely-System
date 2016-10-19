927c79f22b4181eaf48b5e6bf057347073cf49ae
buzai <783243077@qq.com>
2016-10-19 15:12:17 +0800

    hard-fix-my-commit

---
+++ src/app/pages/shejichangshang/jiandianguanli/designfinishedCtrl.js
@@ -0,0 +1,86 @@
+/**
+ * @author v.lugovksy
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.shejichangshang.jiandianguanliss')
+      .controller('designfinishedCtrl', designfinishedCtrl);
+
+  /** @ngInject */
+  function designfinishedCtrl($scope) {
+    /** 代理商概览*/
+    $scope.partners=
+       	{
+       		id:1,
+       		compony:'北京xx',
+       		name:'汽车',
+       		place:'北京',
+       		type:'旗舰店',
+       		person:'黎明',
+       		phone:'13235556556',
+       		email:'1213444@qq.com'
+       	};
+
+    /** 初稿 firstcheckLog*/
+    $scope.firstshowflag=false;
+    $scope.firstChange=function(){
+        $scope.firstshowflag=!$scope.firstshowflag;
+    };
+    $scope.firstcheckLog=[
+         {
+           date:'20161008',
+           person:'黎明',
+           x:'业务部门审核通过',
+           result:'市场部 张经理审核通过'
+         },
+         {
+           date:'20161008',
+           person:'黎明',
+           x:'业务部门审核通过',
+           result:'市场部 张经理审核通过'
+         },
+         {
+           date:'20161008',
+           person:'黎明',
+           x:'业务部门审核通过',
+           result:'市场部 张经理审核通过'
+         }
+
+      ];
+
+      /** 终稿 finalcheckLog*/
+      $scope.finalshowflag=false;
+      $scope.finalChange=function(){
+          $scope.finalshowflag=!$scope.finalshowflag;
+      };
+      $scope.finalcheckLog=[
+           {
+             date:'20161008',
+             person:'黎明',
+             x:'业务部门审核通过',
+             result:'市场部 张经理审核通过'
+           },
+           {
+             date:'20161008',
+             person:'黎明',
+             x:'业务部门审核通过',
+             result:'市场部 张经理审核通过'
+           },
+           {
+             date:'20161008',
+             person:'黎明',
+             x:'业务部门审核通过',
+             result:'市场部 张经理审核通过'
+           }
+
+        ];
+
+        /** 提交审核*/
+        $scope.submit=function(){
+            console.log('提交审核');
+        };
+
+  }
+})();
