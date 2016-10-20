0a4e261b95273e8dde98c806a143c3b54412f6b2
Kibos <759508922@qq.com>
2016-10-20 22:03:46 +0800

    修过 界面

--- src/app/pages/gongyingshang/jiandianguanli/contractglCtrl.js
+++ src/app/pages/gongyingshang/jiandianguanli/contractglCtrl.js
@@ -15,7 +15,7 @@
     {
         id:1,
         area:'北京',
-        company:'维擎科技',
+        company:'北京吉利4S店',
         person:'李明',
         phone:'123456789',
         state:'待上传合同',
@@ -24,7 +24,7 @@
       {
         id:2,
         area:'北京',
-        company:'维擎科技',
+        company:'北京吉利4S店',
         person:'张新华',
         phone:'123456789',
         state:'待首付款确认',
@@ -33,7 +33,7 @@
       {
         id:3,
         area:'北京',
-        company:'维擎科技',
+        company:'北京吉利4S店',
         person:'赵国栋',
         phone:'123456789',
         state:'待供货商发货',
@@ -42,7 +42,7 @@
       {
         id:4,
         area:'北京',
-        company:'维擎科技',
+        company:'北京吉利4S店',
         person:'尚军',
         phone:'123456789',
         state:'待供货商到货确认',
@@ -50,8 +50,8 @@
       },
       {
         id:5,
-        area:'北京',
-        company:'维擎科技',
+        area:'杭州',
+        company:'杭州吉利4S店',
         person:'李煜',
         phone:'123456789',
         state:'待供货商安装完成确认',
@@ -59,8 +59,8 @@
       },
       {
         id:6,
-        area:'北京',
-        company:'维擎科技',
+        area:'杭州',
+        company:'杭州吉利4S店',
         person:'钱多多',
         phone:'123456789',
         state:'待上传合同',
@@ -68,8 +68,8 @@
       },
       {
         id:7,
-        area:'北京',
-        company:'维擎科技',
+        area:'杭州',
+        company:'杭州吉利4S店',
         person:'沈海',
         phone:'123456789',
         state:'待上传合同',
