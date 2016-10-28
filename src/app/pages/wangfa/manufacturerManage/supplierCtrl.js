265c0d4c822eae2976f468c166f1dd47605b9e1d
Kibos <759508922@qq.com>
2016-10-28 15:36:46 +0800

    ff

--- src/app/pages/wangfa/manufacturerManage/supplierCtrl.js
+++ src/app/pages/wangfa/manufacturerManage/supplierCtrl.js
@@ -10,26 +10,6 @@
 
   /** @ngInject */
   function supplierCtrl($scope,$state,$stateParams,$rootScope,RoleUser,toastr,Supplier) {
-<<<<<<< HEAD
-    $scope.smartTablePageSize = 5;
-    $rootScope.sidebartopfalg = false;
-    $rootScope.loginflag = true ;
-
-    	$scope.add=function(){
-    		$state.go('ba.hezuohuoban.add',{obj:2});
-    	};
-
-
-   var entries =RoleUser.gongUser.query(function() {
-      console.log(entries);
-      $scope.suppliers=entries;
-      
-
-
-
-    //   console.log("si=="+suppliers.length);
-  });
-=======
     $rootScope.sidebartopfalg = false;
     $rootScope.loginflag = true ;
 
@@ -54,6 +34,5 @@
         // vm.suppliers=entries;
       //   console.log("si=="+suppliers.length);
     });
->>>>>>> ca77ec99a1025a72afa99e2fec3575dfc61010cb
   }
 })();
