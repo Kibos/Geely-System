d5855c916d1ff1bb9d19e57130c467fa1967167c
81128786 <2451278782@qq.com>
2016-10-29 18:18:14 +0800

    'sxx---shejishang'

--- src/app/pages/wangfa/manufacturerManage/manufacturerManageCtrl.js
+++ src/app/pages/wangfa/manufacturerManage/manufacturerManageCtrl.js
@@ -9,14 +9,29 @@
       .controller('ManufacturerManageCtrl', ManufacturerManageCtrl);
 
   /** @ngInject */
-  function ManufacturerManageCtrl($scope,$state,$stateParams,$rootScope,Supplier,RoleUser) {
+  function ManufacturerManageCtrl($scope,$state,$stateParams,$rootScope,Supplier,RoleUser,toastr) {
     $rootScope.sidebartopfalg = false;
     $rootScope.loginflag = true ;
+    $scope.smartTablePageSize = 5;
 
-
+    // 添加
     $scope.add=function(){
-    		$state.go('ba.hezuohuoban.add',{obj:1});
-    }
+       $state.go('ba.hezuohuoban.add',{obj:2});
+     };
+    // 删除
+    // $scope.remove=function(index){
+    //   RoleUser.gongUser.delete($scope.designManufacturers[index]);
+    //   $scope.designManufacturers.splice(index, 1);
+    //   toastr.success('删除成功!');
+    // };
+    $scope.remove= function(index) {
+      $scope.designManufacturers.splice(index, 1);
+    };
+
+    // 查看
+    $scope.see=function(item){
+      $state.go('ba.hezuohuoban.sjsxx',{obj:item});
+    };
 
 //         var entries =RoleUser.gongUser.query(function() {
 //          console.log(entries);
@@ -29,7 +44,6 @@
     			name:'A公司',
     			address:'杭州市',
           ownName:'张恒',
-    			person:'张三',
     			phone:'12344665689',
     			createAt:'20161010',
     			number:'10'
@@ -38,7 +52,6 @@
             name:'杭州吉利4S店',
             address:'杭州市',
             ownName:'李恒',
-            person:'张三',
             phone:'12344665689',
             createAt:'20161010',
             number:'10'
@@ -47,7 +60,6 @@
             name:'北京吉利4S店',
             address:'杭州市',
             ownName:'王璇',
-            person:'张三',
             phone:'12344665689',
             createAt:'20161010',
             number:'10'
@@ -56,7 +68,6 @@
             name:'北京吉利4S店',
             address:'杭州市',
             ownName:'单军',
-            person:'张三',
             phone:'12344665689',
             createAt:'20161010',
             number:'10'
@@ -65,7 +76,6 @@
             name:'北京吉利4S店',
             address:'杭州市',
             ownName:'刘健',
-            person:'张三',
             phone:'12344665689',
             createAt:'20161010',
             number:'10'
@@ -74,11 +84,12 @@
             name:'北京吉利4S店',
             address:'杭州市',
             ownName:'金多多',
-            person:'张三',
             phone:'12344665689',
             createAt:'20161010',
             number:'10'
         }
-    	]
+    	];
+
+
   }
 })();
